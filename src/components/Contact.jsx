802ae6f6b4986ad6752077ff3b925579f5e5c581
Md.Mahfuz Hossain <118552884+radifkhanrafin@users.noreply.github.com>
2025-11-18 04:00:12 +0600

    complete update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
   };
 
   return (
-    <div className='xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden'>
+    <div className='xl:mt-12 flex xl:flex-row flex-col-reverse items-center gap-10 overflow-hidden'>
       <motion.div
         variants={slideIn("down", "tween", 0.2, 2)}
         className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
@@ -32,7 +32,7 @@ const Contact = () => {
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
 
-        <div className='mt-12 flex flex-col gap-8'>
+        <div className='mt-12 flex flex-col    gap-8'>
           <p className='text-white'>
             Click the button below to contact me directly via WhatsApp:
           </p>
@@ -48,7 +48,7 @@ const Contact = () => {
 
       <motion.div
         variants={slideIn("right", "tween", 0.2, 1)}
-        className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
+        className='xl:flex-1 xl:h-[650px] md:h-[550px] h-[350px]'
       >
         <EarthCanvas />
       </motion.div>
